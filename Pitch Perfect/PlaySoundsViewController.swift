c25490f452e16c2cc296d8fdd6935a29a7e33d1e
me <me@mes-MacBook-Pro.local>
2015-04-04 14:39:38 -0400

    4b

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -11,17 +11,25 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     var audioPlayer:AVAudioPlayer!
-    
+    var receivedAudio:RecordedAudio!
+
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()/*
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
             var filePathUrl = NSURL.fileURLWithPath(filePath)
-            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
-            audioPlayer.enableRate = true
         }
         else{
           println("no audio file")
         }
+*/
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
+
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,21 +38,54 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSlowAudio(sender:UIButton){
-        audioPlayer.stop()
+        stopAll()
         audioPlayer.rate = 0.5
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
    
     @IBAction func playFastAudio(sender: UIButton) {
-        audioPlayer.stop()
+        stopAll()
         audioPlayer.rate = 1.5
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
 
-    @IBAction func stopAudio(sender: UIButton) {
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+
+    @IBAction func playDarthvaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+
+    func playAudioWithVariablePitch(pitch: Float){
+        stopAll()
+
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+
+    func stopAll(){
         audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        stopAll()
     }
 
     /*
